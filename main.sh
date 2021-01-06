4f83da70b5deb61da6a2bbe90d831c2e1353f936
wujun8 <winston8.2013@outlook.com>
2021-01-06 17:48:29 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,5 +2,9 @@ title="$(date +"%F %H:%M:%S") CCCAT checkin"
 echo $title
 curlr=$(curl -sS "https://cccat.io/user/_checkin.php" -H "authority: cccat.io" -H "accept: application/json, text/javascript, */*; q=0.01" -H "dnt: 1" -H "x-requested-with: XMLHttpRequest" -H "user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36" -H "sec-fetch-site: same-origin" -H "sec-fetch-mode: cors" -H "sec-fetch-dest: empty" -H "referer: https://cccat.io/user/index.php" -H "accept-language: zh-CN,zh;q=0.9,en;q=0.8,zh-TW;q=0.7" -H "cookie: $CK" --compressed)
 echo $curlr
+if [ -z "$curlr" ] ; then
+    curlr="something is wrong"
+    echo $curlr
+fi
 curlr="CCCAT checkin $curlr"
 curl -s -G "https://sc.ftqq.com/$SCKEY.send" --data-urlencode "text=$curlr" --data-urlencode "desp=$curlr"
