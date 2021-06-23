139a82f4d53ff6936fe2331a7eefecde187cac5a
wujun8 <winston8.2013@outlook.com>
2021-06-23 17:00:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,9 +1,8 @@
 title="$(date +"%F %H:%M:%S") CCCAT checkin"
 echo $title
 today=$(date +"%F")
-run_today=$(< runday) || run_today=0
 
-if [[ "$today" != "$run_today" ]] ; then
+if [[ "$today" != $(< run_today) ]] ; then
     curlr=$(python checkin.py)
     echo $curlr
     if [ -z "$curlr" ] ; then
@@ -13,7 +12,7 @@ if [[ "$today" != "$run_today" ]] ; then
     curlr=$(echo -e "CCCAT checkin $curlr" | sed 's/["\{}]//g')
     #curl -s -G "https://sc.ftqq.com/$SCKEY.send" --data-urlencode "text=$curlr" --data-urlencode "desp=$curlr"
     curl -sS "https://oapi.dingtalk.com/robot/send?access_token=$DDTOKEN" -X "POST" -H "Accept: application/json;charset=utf-8" -H "Content-Type: application/json;charset=utf-8" -d '{"msgtype": "text","text": {"content": "'"$curlr"'"}}' --compressed
-    echo $today > runday
+    echo $today > run_today
 else
     echo already run today @$today
     exit
